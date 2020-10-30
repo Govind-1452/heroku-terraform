ecd30a287421c2b10caf60779bea032e1a2e1b22
Govind Choudhary <gchoudhary@govindch-ltm.internal.salesforce.com>
2020-10-30 16:53:57 +0530

    test terraform

--- test1.tf
+++ test1.tf
@@ -3,12 +3,12 @@ provider "heroku" {
   api_key = "b4385c10-cd8c-4d39-9c67-65042530ae3f"
 }
 resource "heroku_app" "this" {
-  name = "my-cal-heroku"
+  name = "my-cal-heroku1"
   region = "us"
 }
 resource "heroku_build" "this" {
   app        = heroku_app.this.id
-  buildpacks = ["https://github.com/heroku/heroku-buildpack-java"]
+  buildpacks = ["https://github.com/heroku/heroku-buildpack-java.git"]
 
   source = {
     url     = "https://github.com/Govind-1452/heroku-terraform.git"
