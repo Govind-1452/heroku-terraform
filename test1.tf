f43aa007ca4527d55984aceb30e3e2f21daaea92
Govind Choudhary <gchoudhary@govindch-ltm.internal.salesforce.com>
2020-11-02 12:14:35 +0530

    test

--- test1.tf
+++ test1.tf
@@ -1,16 +1,17 @@
 provider "heroku" {
   email   = "gchoudhary@salesforce.com"
-  api_key = "b4385c10-cd8c-4d39-9c67-65042530ae3f"
+  api_key = "41bcfac2-79cb-4cac-b326-e5835cc1ddfd"
 }
 resource "heroku_app" "this" {
-  name = "my-cal-heroku1"
+  name = "my-cal-heroku11"
   region = "us"
 }
 resource "heroku_build" "this" {
   app        = heroku_app.this.id
   buildpacks = ["https://github.com/heroku/heroku-buildpack-java.git"]
-
   source = {
+    # This app uses a community buildpack, set it in `buildpacks` above.
     url     = "https://github.com/Govind-1452/heroku-terraform.git"
+
   }
 }
